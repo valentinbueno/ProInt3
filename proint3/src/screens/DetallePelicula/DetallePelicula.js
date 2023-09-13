e4ddfefcd52423d31b31e2493c7cb7215a05dd88
SantiagoVillares <svillares@udesa.edu.ar>
2023-09-13 16:20:39 -0300

    detalle casi completo

--- proint3/src/screens/DetallePelicula/DetallePelicula.js
+++ proint3/src/screens/DetallePelicula/DetallePelicula.js
@@ -4,14 +4,44 @@ class DetallePelicula extends Component{
     constructor(props){
         super(props)
         this.state={
-            id:this.props.match.params.id
+            id:this.props.match.params.id,
+            peliculasPop:[]
         }
     }
-    render(){
-        console.log(this.state.id);
-        return(
-            <p>{this.state.id}</p>
-        )
+    componentDidMount(){
+        let idMovie= this.state.id
+        fetch(`https://api.themoviedb.org/3/movie/${idMovie}?api_key=5af2599bc48eedc0c872d98ac992b8e3`)
+            .then((res) => res.json())
+            .then((data) =>
+                this.setState({
+                peliculasPop: data,
+                })
+            )
+            .catch();
     }
-}
+    render(){
+        console.log(this.state.peliculasPop);
+        return(
+            <React.Fragment>
+                <p className="nombre_categoria">{this.state.peliculasPop.title}</p>
+                <div>
+                    <img src={`https://image.tmdb.org/t/p/w500/${this.state.peliculasPop.poster_path}`} alt="imagenPelicula"/>
+                </div>
+                <div>
+                    <ul>
+                    <li>Valoracion:{this.state.peliculasPop.vote_average}</li>
+                    <li>Fecha de Estreno:{this.state.peliculasPop.release_date}</li>
+                    <li>Duracion:{this.state.peliculasPop.runtime} mins</li>
+                    <li>Sinopsis:{this.state.peliculasPop.overview}</li>
+                    <ul>Genero:
+                        <li>{this.state.peliculasPop.genres.map((generos, idx) => {
+                            return (generos=generos.name )
+                            })}
+                        </li>
+                    <ul/>
+                </div>
+            </React.Fragment>
+        );
+    
+
 export default DetallePelicula
